9a44cb2f73fe817d3ab671ff908b766351f84da5
Bas <b_kalkman@hotmail.com>
2021-04-17 23:45:11 +0200

    10001st prime

---
+++ 10001-prime.py
@@ -0,0 +1,16 @@
+primes = [2]
+
+def is_prime(i):
+  for num in primes:
+    if i % num == 0:
+      return False
+  return True
+
+i = 3
+while len(primes) < 10001:
+  if is_prime(i):
+    primes.append(i)
+  i += 2
+
+print(primes[-1])
+
